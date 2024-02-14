fec160841a48c4c41e95c1b29d24fd5af957d33d
Kendry R. Soto <kendryrsoto@gmail.com>
2024-02-13 23:58:52 -0400

    functionality is added to each character passwordComplete function

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ let uppercase = document.querySelector('#uppercase');
 let numbers = document.querySelector('#numbers');
 let symbols = document.querySelector('#symbols');
 
+let passwordAll = '';
+
 const  lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
 const  uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const  numbersList = '0123456789';
@@ -18,10 +20,45 @@ function addEventListener(){
     button.addEventListener('click', generatePassword);
 }
 
+function getRandonNumber(max) {
+    return Math.floor(Math.random() * max);
+}
 
-function generatePassword() {
-    console.log('enter');}
+function generatePassword(e) {
+    passwordAll = '';
+    if(lowercase.checked){
+        passwordAll += getLowerCase();
+    }
+    if(uppercase.checked){
+        passwordAll += getUpperCase();
+    }
+    if(numbers.checked){
+        passwordAll += getNumber();
+    }
+    if(symbols.checked){
+        passwordAll += getSymbol();
+    }
+    console.log(passwordAll);
+}
 
+function passwordComplete() {
+    while(passwordAll.length < parseInt(displayLength.textContent)){
+        passwordAll += getRandonPassword();
+    }
+}
+
+function getUpperCase() {
+    return uppercaseLetters[getRandonNumber(uppercaseLetters.length)];
+}
+function getLowerCase() {
+    return lowercaseLetters[getRandonNumber(lowercaseLetters.length)];
+}
+function getNumber() {
+    return numbersList[getRandonNumber(numbersList.length)];
+}
+function getSymbol() {
+    return symbolsList[getRandonNumber(symbolsList.length)];
+}
 
 function showValue(value) {
     displayLength.textContent = value;
